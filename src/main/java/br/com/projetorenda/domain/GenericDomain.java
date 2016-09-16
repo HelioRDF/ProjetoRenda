c84776fedc3afd679ee1ff4e8544518cfff3181a
Helio Franca <heliordf@hotmail.com>
2016-09-16 18:38:41 -0300

    atualização

---
+++ src/main/java/br/com/projetorenda/domain/GenericDomain.java
@@ -0,0 +1,24 @@
+package br.com.projetorenda.domain;
+
+import java.io.Serializable;
+
+public class GenericDomain implements Serializable {
+	
+private static final long serialVersionUID = -5839140217941489819L;
+
+private Long codigo;
+
+
+
+public Long getCodigo() {
+	return codigo;
+}
+
+public void setCodigo(Long codigo) {
+	this.codigo = codigo;
+}	
+
+
+
+
+}
